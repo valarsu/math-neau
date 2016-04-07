1b0d1493f7168fa15d850bfa8d0b8f936cf8a805
valarsu <valar_su@163.com>
2016-04-08 00:55:44 +0800

    添加fontawesome并且修改公共头部样式

--- templets/default/js/pic_scroll.js
+++ templets/default/js/pic_scroll.js
@@ -1,47 +1,46 @@
-
 (function () {
     $.fn.infiniteCarousel = function () {
         function repeat(str, n) {
-            return new Array( n + 1 ).join(str);
+            return new Array(n + 1).join(str);
         }
-        
+
         return this.each(function () {
             // magic!
             var $wrapper = $('> div', this).css('overflow', 'hidden'),
                 $slider = $wrapper.find('> ul').width(9999),
                 $items = $slider.find('> li'),
                 $single = $items.filter(':first')
-                
-                singleWidth = $single.outerWidth(),
+
+            singleWidth = $single.outerWidth(),
                 visible = Math.ceil($wrapper.innerWidth() / singleWidth),
                 currentPage = 1,
                 pages = Math.ceil($items.length / visible);
-                
+
             /* TASKS */
-            
+
             // 1. pad the pages with empty element if required
             if ($items.length % visible != 0) {
                 // pad
                 $slider.append(repeat('<li class="empty" />', visible - ($items.length % visible)));
                 $items = $slider.find('> li');
             }
-            
+
             // 2. create the carousel padding on left and right (cloned)
             $items.filter(':first').before($items.slice(-visible).clone().addClass('cloned'));
             $items.filter(':last').after($items.slice(0, visible).clone().addClass('cloned'));
             $items = $slider.find('> li');
-            
+
             // 3. reset scroll
             $wrapper.scrollLeft(singleWidth * visible);
-            
+
             // 4. paging function
             function gotoPage(page) {
                 var dir = page < currentPage ? -1 : 1,
                     n = Math.abs(currentPage - page),
                     left = singleWidth * dir * visible * n;
-                
+
                 $wrapper.filter(':not(:animated)').animate({
-                    scrollLeft : '+=' + left
+                    scrollLeft: '+=' + left
                 }, 500, function () {
                     // if page == last page - then reset position
                     if (page > pages) {
@@ -51,29 +50,29 @@
                         page = pages;
                         $wrapper.scrollLeft(singleWidth * visible * pages);
                     }
-                    
+
                     currentPage = page;
                 });
             }
-            
+
             // 5. insert the back and forward link
             $wrapper.after('<a href="#" class="arrow back">&lt;</a><a href="#" class="arrow forward">&gt;</a>');
-            
+
             // 6. bind the back and forward links
             $('a.back', this).click(function () {
                 gotoPage(currentPage - 1);
                 return false;
             });
-            
+
             $('a.forward', this).click(function () {
                 gotoPage(currentPage + 1);
                 return false;
             });
-            
+
             $(this).bind('goto', function (event, page) {
                 gotoPage(page);
             });
-            
+
             // THIS IS NEW CODE FOR THE AUTOMATIC INFINITE CAROUSEL
             $(this).bind('next', function () {
                 gotoPage(currentPage + 1);
@@ -85,8 +84,7 @@
 $(document).ready(function () {
     // THIS IS NEW CODE FOR THE AUTOMATIC INFINITE CAROUSEL
     var autoscrolling = true;
-    if($('#imgscroll > li').length>0)
-    {
+    if ($('#imgscroll > li').length > 0) {
         $('.infiniteCarousel').infiniteCarousel().mouseover(function () {
             autoscrolling = false;
         }).mouseout(function () {
@@ -99,5 +97,5 @@ $(document).ready(function () {
         }, 5000);
 
     }
-    
+
 });
